b8e4b4a8d44191baa42b57b4d44700a012bd62e3
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-05-31 20:54:15 +0530

    Adding binning

--- coinflip.py
+++ coinflip.py
@@ -1,5 +1,6 @@
 import random as rnd
-import matplotlib.pyplot as plt
+import numpy
+#import matplotlib.pyplot as plt
 
 def minIndex(list):
 	minimum= min(list)
@@ -22,10 +23,13 @@ for k in range(100000):
 
 	nu1.append(headFreq[0])
 	nuRand.append(headFreq[rnd.choice(range(1000))])
-	nuMin.append(minIndex(headFreq))
+	nuMin.append(headFreq[minIndex(headFreq)])
 	print(k)
 
 print(sum(nuMin)/100000)
-plt.hist(nu1)
-plt.hist(nuRand)
-plt.hist(nuMin)
+nu1Dist=numpy.histogram(nu1,bins=11)
+nuRandDist=numpy.histogram(nuRand,bins=11)
+nuMinDist=numpy.histogram(nuMin,bins=11)
+print(nu1Dist[0])
+print(nuRandDist)
+print(nuMinDist)
