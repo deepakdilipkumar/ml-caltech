c5c221159eee174a754a9818a787b4b28c63137e
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-20 18:21:41 +0530

    Adding g=ax^2

--- biasvariancegeneral.py
+++ biasvariancegeneral.py
@@ -12,13 +12,13 @@ def a(x1,x2):
 	#return ((x1*f(x1)+x2*f(x2))/(pow(x1,2)+pow(x2,2)))			# Parameter minimizing mean squared error for given dataset of 2 points for g=ax
 	#return 0													# g=b
 	#return ((f(x2)-f(x1))/(x2-x1))								# g=ax+b
-
+	return ((pow(x1,2)*f(x1)+pow(x2,2)*f(x2))/(pow(x1,4)+pow(x2,4)))	# g=ax^2
 
 def b(x1,x2):
 	#return 0 													# g=ax
 	#return (f(x1)+f(x2))/2										# g=b
 	#return ((f(x1)*x2-f(x2)*x1)/(x2-x1))						# g=ax+b
-
+	return 0													# g=ax^2
 
 numdata=100000
 abar=0
@@ -47,11 +47,11 @@ bias=0
 
 for i in range(numx):
 	x = point()
-	errbias+= pow(abar*x+bbar-f(x),2)
+	errbias+= pow(abar*pow(x,2)+bbar-f(x),2)
 	for j in range(numd):
 		x1=point()
 		x2=point()
-		errvar+=pow(a(x1,x2)*x+b(x1,x2)-abar*x-bbar,2)
+		errvar+=pow(a(x1,x2)*pow(x,2)+b(x1,x2)-abar*pow(x,2)-bbar,2)
 
 	#print(i)
 	errvar/=numd
