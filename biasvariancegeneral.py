0f154b5ca0aff53d5405bf0911dac6ff216a423e
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-06-20 18:28:45 +0530

    Adding g=ax^2+b

--- biasvariancegeneral.py
+++ biasvariancegeneral.py
@@ -8,17 +8,19 @@ def point():
 def f(x):
 	return math.sin(math.pi*x)
 
-def a(x1,x2):													
-	#return ((x1*f(x1)+x2*f(x2))/(pow(x1,2)+pow(x2,2)))			# Parameter minimizing mean squared error for given dataset of 2 points for g=ax
-	#return 0													# g=b
-	#return ((f(x2)-f(x1))/(x2-x1))								# g=ax+b
-	return ((pow(x1,2)*f(x1)+pow(x2,2)*f(x2))/(pow(x1,4)+pow(x2,4)))	# g=ax^2
+def a(x1,x2):														
+	#return ((x1*f(x1)+x2*f(x2))/(pow(x1,2)+pow(x2,2)))					# Parameter minimizing mean squared error for given dataset of 2 points for g=ax
+	#return 0															# g=b
+	#return ((f(x2)-f(x1))/(x2-x1))										# g=ax+b
+	#return ((pow(x1,2)*f(x1)+pow(x2,2)*f(x2))/(pow(x1,4)+pow(x2,4)))	# g=ax^2
+	return (f(x2)-f(x1))/(pow(x2,2)-pow(x1,2))							# g=ax^2 +b
 
 def b(x1,x2):
-	#return 0 													# g=ax
-	#return (f(x1)+f(x2))/2										# g=b
-	#return ((f(x1)*x2-f(x2)*x1)/(x2-x1))						# g=ax+b
-	return 0													# g=ax^2
+	#return 0 														# g=ax
+	#return (f(x1)+f(x2))/2											# g=b
+	#return ((f(x1)*x2-f(x2)*x1)/(x2-x1))							# g=ax+b
+	#return 0														# g=ax^2
+	return (f(x1)*pow(x2,2)-f(x2)*pow(x1,2))/(pow(x2,2)-pow(x1,2))	# g=ax^2+b
 
 numdata=100000
 abar=0
@@ -48,10 +50,12 @@ bias=0
 for i in range(numx):
 	x = point()
 	errbias+= pow(abar*pow(x,2)+bbar-f(x),2)
+#	errbias+= pow(abar*x+bbar-f(x),2)
 	for j in range(numd):
 		x1=point()
 		x2=point()
 		errvar+=pow(a(x1,x2)*pow(x,2)+b(x1,x2)-abar*pow(x,2)-bbar,2)
+#		errvar+=pow(a(x1,x2)*x+b(x1,x2)-abar*x-bbar,2)
 
 	#print(i)
 	errvar/=numd
