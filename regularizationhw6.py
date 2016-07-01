345ded33f1d7b70d2693791010400f3eb9ab39a2
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-01 21:51:51 +0530

    Bug fix

--- regularizationhw6.py
+++ regularizationhw6.py
@@ -5,56 +5,45 @@ import csv
 def label(weights,input):
 	return np.sign(weights.dot(input))
 
+traindata=np.genfromtxt("hw6in.csv")
+testdata=np.genfromtxt("hw6out.csv")			
 
-train_file = csv.reader(open('hw6in.csv', 'rb')) 	
-test_file = csv.reader(open('hw6out.csv', 'rb')) 	
-traindata=[] 	
-testdata=[]											
+numtrain=35
+numtest=250				
 
-numtrain=0
-numtest=0
-
-for row in train_file: 							
-    traindata.append(row) 	
-    numtrain+=1							
-traindata = np.array(traindata) 	
-
-for row in test_file: 							
-    testdata.append(row[0:]) 	
-    numtest+=1							
-testdata = np.array(testdata) 									
-
-inputlabels=traindata[,2]
-
+inputlabels=traindata[:,2]
+transformedtraindata=np.array([0,0,0,0,0,0,0,0])
 for i in range(numtrain):
 	x1=traindata[i,0]
 	x2=traindata[i,1]
-
 	transform=np.array([1,x1,x2,pow(x1,2),pow(x2,2),x1*x2,abs(x1-x2),abs(x1+x2)])
-	transformedtraindata.append(transform)
+	transformedtraindata=np.vstack((transformedtraindata,transform))
+
+transformedtraindata=transformedtraindata[1:,:]
 
 weights = np.dot(np.dot(np.linalg.inv(np.dot(transformedtraindata.T,transformedtraindata)),transformedtraindata.T),inputlabels)
 	
-ein=0
+ein=0.0
 
 for i in range(numtrain):
-	if label(weights,transformedtraindata[i,0:2])!=inputlabels[i]:
+	if label(weights,transformedtraindata[i,:])!=inputlabels[i]:
 		ein+=1
 
 ein/=numtrain
 print(ein)
 
-outputlabels=testdata[,2]
+outputlabels=testdata[:,2]
+transformedtestdata=np.array([0,0,0,0,0,0,0,0])
 for i in range(numtest):
 	x1=testdata[i,0]
 	x2=testdata[i,1]
-
 	transform=np.array([1,x1,x2,pow(x1,2),pow(x2,2),x1*x2,abs(x1-x2),abs(x1+x2)])
-	transformedtestdata.append(transform)
+	transformedtestdata=np.vstack((transformedtestdata,transform))
 
-eout =0
+transformedtestdata=transformedtestdata[1:,:]
+eout =0.0
 for i in range(numtest):
-	if label(weights,transformedtestdata[i,0:2])!=outputlabels[i]:
+	if label(weights,transformedtestdata[i,:])!=outputlabels[i]:
 		eout+=1
 
 eout/=numtest
