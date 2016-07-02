d2d938cf5efbefc3250e034614e2bbaeaf3c4fd7
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-02 17:18:31 +0530

    Ein and Eout for a range of reg. parameters

--- regularizationhw6.py
+++ regularizationhw6.py
@@ -21,20 +21,6 @@ for i in range(numtrain):
 
 transformedtraindata=transformedtraindata[1:,:]
 
-k=-1
-regparameter=pow(10,k)
-
-weights = np.dot(np.dot(np.linalg.inv(np.dot(transformedtraindata.T,transformedtraindata)+regparameter*np.eye(8)),transformedtraindata.T),inputlabels)
-	
-ein=0.0
-
-for i in range(numtrain):
-	if label(weights,transformedtraindata[i,:])!=inputlabels[i]:
-		ein+=1
-
-ein/=numtrain
-print(ein)
-
 outputlabels=testdata[:,2]
 transformedtestdata=np.array([0,0,0,0,0,0,0,0])
 for i in range(numtest):
@@ -44,12 +30,36 @@ for i in range(numtest):
 	transformedtestdata=np.vstack((transformedtestdata,transform))
 
 transformedtestdata=transformedtestdata[1:,:]
-eout =0.0
-for i in range(numtest):
-	if label(weights,transformedtestdata[i,:])!=outputlabels[i]:
-		eout+=1
 
-eout/=numtest
-print(eout)
+einlist=[]
+eoutlist=[]
+klist=[]
 
-print(weights)
+for i in range(100):
+	k=(i-50)/10
+	klist.append(k)
+	regparameter=pow(10,k)
+
+	weights = np.dot(np.dot(np.linalg.inv(np.dot(transformedtraindata.T,transformedtraindata)+regparameter*np.eye(8)),transformedtraindata.T),inputlabels)
+		
+	ein=0.0
+	for i in range(numtrain):
+		if label(weights,transformedtraindata[i,:])!=inputlabels[i]:
+			ein+=1
+
+	ein/=numtrain
+	einlist.append(ein)
+	#print(ein)
+
+	eout =0.0
+	for i in range(numtest):
+		if label(weights,transformedtestdata[i,:])!=outputlabels[i]:
+			eout+=1
+
+	eout/=numtest
+	eoutlist.append(eout)
+#print(eout)
+
+
+#print(einlist)
+#print(eoutlist)
