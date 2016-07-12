e96bdc6a1d77682c0bf60cade98f92170ee4190f
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-12 18:40:05 +0530

    Correcting node value calculation

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -12,6 +12,7 @@ layersizearray = [2,2,2,1]			# Input, Hidden layers, output
 numlayers=3
 learning=0.1
 maxnodes=max(layersizearray)
+
 w = [ [ [0]* (numlayers+1) ] * (maxnodes+1) ] * (maxnodes+1)
 x = [ [0]* (numlayers+1)] * (maxnodes+1)
 #x[:][0]=inputfeatures
@@ -26,6 +27,6 @@ for l in range(numlayers)[1:]:
 	for j in range(layersize(l)):
 		signal=0
 		for i in range(layersize(l-1)):
-			signal+=x[i][l]*w[i][j][l]
+			signal+=x[i][l-1]*w[i][j][l-1]
 
 		x[j][l]=nl(signal)
