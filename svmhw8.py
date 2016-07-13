d38b921c6b30ae54dc540531211e137c19c1b3b4
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-13 18:45:23 +0530

    Setting up svm class for HW8 exercises

---
+++ svmhw8.py
@@ -0,0 +1,47 @@
+from sklearn import svm
+import numpy as np 
+
+class supportvectormachine:
+ 	'End to end svm class'
+
+ 	def __init__(self, kernel='rbf', C=1, coeff=1, degree=3, gamma =1 ):
+ 		self.kernel = kernel
+ 		self.C = C
+ 		self.coeff = coeff
+ 		self.degree = degree
+ 		self.gamma = gamma
+
+ 	def updatekernel(self,kernel):
+ 		self.kernel = kernel
+
+ 	def updateC(self,C):
+ 		self.C = C
+
+ 	def updatecoeff(self,coeff):
+ 		self.coeff = coeff
+
+ 	def updatedegree(self,degree):
+ 		self.degree = degree
+
+ 	def updategamma(self,gamma):
+ 		self.gamma = gamma
+
+ 	def trainset(self,traindata):
+ 		self.traindata = traindata
+
+ 	def testset(self,testdata):
+ 		self.testdata = testdata
+
+ 	def model(self):
+ 		print "Kernel: ", self.kernel, "\nPenalty Parameter: ", self.C, "\nDegree of Poly Kernel: ", self.degree, " \nKernel coefficient: ", self.gamma, "\nIndependant term in kernel: ", self.coeff
+
+
+
+traindata=np.genfromtxt("hw8train.txt")
+testdata=np.genfromtxt("hw8test.txt")
+
+svm1 = supportvectormachine('rbf', 1, 1, 10, 1)
+svm1.trainset(traindata)
+svm1.testset(testdata)
+svm1.model()
+
