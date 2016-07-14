066f4158546439626cfa4c094ca591203e6c4c1a
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2016-07-14 18:09:26 +0530

    Changing default param values, array indexing

--- svmhw8.py
+++ svmhw8.py
@@ -4,7 +4,7 @@ import numpy as np
 class supportvectormachine:
  	'End to end svm class'
 
- 	def __init__(self, kernel='rbf', C=1, coeff=1, degree=3, gamma =1 ):
+ 	def __init__(self, kernel='rbf', C=1, coeff=0, degree=3, gamma =0.5 ):
  		self.kernel = kernel
  		self.C = C
  		self.coeff = coeff
@@ -46,55 +46,82 @@ class supportvectormachine:
 
  	def onevsall(self,digit):
  		for i in range(np.shape(self.traindata)[0]):
- 			if self.traindata[i][0]==digit:
- 				self.traindata[i][0]=1
+ 			if self.traindata[i,0]==digit:
+ 				self.traindata[i,0]=1
  			else:
- 				self.traindata[i][0]=-1
+ 				self.traindata[i,0]=-1
+
+ 		for i in range(np.shape(self.testdata)[0]):
+ 			if self.testdata[i,0]==digit:
+ 				self.testdata[i,0]=1
+ 			else:
+ 				self.testdata[i,0]=-1
 
  	def onevsone(self,digit1,digit2):
  		for i in range(np.shape(self.traindata)[0]):
- 			if self.traindata[i][0]==digit1:
- 				self.traindata[i][0]=1
- 			elif self.traindata[i][0]==digit2:
- 				self.traindata[i][0]=-1 
+ 			if self.traindata[i,0]==digit1:
+ 				self.traindata[i,0]=1
+ 			elif self.traindata[i,0]==digit2:
+ 				self.traindata[i,0]=-1 
  			else:
- 				self.traindata[i][0]=0	
+ 				self.traindata[i,0]=0	
+
+ 		for i in range(np.shape(self.testdata)[0]):
+ 			if self.testdata[i,0]==digit1:
+ 				self.testdata[i,0]=1
+ 			elif self.testdata[i,0]==digit2:
+ 				self.testdata[i,0]=-1 
+ 			else:
+ 				self.testdata[i,0]=0	
+
 
  	def train(self):
  		self.classifier.fit(self.traindata[:,1:],self.traindata[:,0])
 
  	def ein(self):
  		self.inprediction = self.classifier.predict(self.traindata[:,1:])
- 		total = len(self.inprediction)
- 		count=0.0
- 		for i in range(total):
+ 		self.total = len(self.inprediction)
+ 		self.count=0.0
+ 		for i in range(self.total):
  			if(self.inprediction[i]!=self.traindata[i,0]):
- 				count+=1
+ 				self.count+=1
 
- 		return count/total
+ 		return self.count/self.total
+ 		#return self.classifier.score(self.traindata[:,1:],self.traindata[:,0])
 
  	def eout(self):
 		self.outprediction = self.classifier.predict(self.testdata[:,1:])
-		total = len(self.outprediction)
-		count=0.0
-		for i in range(total):
+		self.total = len(self.outprediction)
+		self.count=0.0
+		for i in range(self.total):
 			if(self.outprediction[i]!=self.testdata[i,0]):
-				count+=1
+				self.count+=1
 
-		return count/total
-
+		return self.count/self.total
+		#return self.classifier.score(self.testdata[:,1:],self.testdata[:,0])
 
 
 
 traindata=np.genfromtxt("hw8train.txt")
 testdata=np.genfromtxt("hw8test.txt")
 
-svm1 = supportvectormachine('rbf', 1, 1, 10, 1)
+svm1 = supportvectormachine('poly', C=0.01, degree=2)
 svm1.trainset(traindata)
 svm1.testset(testdata)
+svm1.onevsall(6)
 svm1.model()
 #svm1.printtrain()
 svm1.train()
-print(svm1.ein())
-print(svm1.eout())
+print svm1.ein(), " ", svm1.eout()
+
+#for i in [0,2,4]:
+#	print(i)
+#	svm2 = supportvectormachine('poly', C=0.01, degree=2)
+#	svm2.trainset(traindata)
+#	svm2.testset(testdata)
+#	svm2.onevsall(0)
+#	svm2.model()
+	#svm1.printtrain()
+#	svm2.train()
+#	print svm2.ein(), " ", svm2.eout()
 
